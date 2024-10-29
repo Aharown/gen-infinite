b36a64082ad8615e50a51d86436f8d75eee18c02
Henri Bellet <henri.clau.bellet@gmail.com>
2024-10-29 20:53:11 +0100

    Post controller added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,56 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.post.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post has been updated ✅."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path, notice: "Post has been deleted 🗑️."
+    else
+      redirect_to posts_path, alert: "Failed to delete the post 🛑."
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :category, photos: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 end
