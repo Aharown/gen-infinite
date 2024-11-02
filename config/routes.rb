f2604e1ed96554085d3d63155307de3f1bcd6796
Aaron Boakye <cheminko@hotmail.com>
2024-11-02 19:35:40 +0000

    Post index for category specific posts and user feed complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :posts
+  resources :posts do
+    get 'category/:category_id', to: 'posts#index', as: :category_posts, on: :collection
+  end
   resources :users, only: [:show]
   resources :tags, except: [:edit, :update]
   resources :categories, only: [:show]
-  resources :answers 
+  resources :answers
 
   # Defines the root path route ("/")
   # root "posts#index"
